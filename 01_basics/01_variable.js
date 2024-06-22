e81ef30274167b94cc0a381dacfe28433e48785f
imsachinkumar <122716893+imsachinkumar@users.noreply.github.com>
2024-06-22 16:57:27 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId=32322
+let accountemail="sachinkumar@gmail.com"
+var accountpassword="2323"
+
+
+console.log(accountId);
+
+console.table([accountId,accountemail,accountpassword])
+
+
+// not use var only use let for varible declaration
+//value of const can not be changed
+
