1845fc007d21b745bf7f8475174394d2d53bdeb5
imsachinkumar <122716893+imsachinkumar@users.noreply.github.com>
2024-06-24 16:56:19 +0000

    math

---
+++ 01_basics/08_numbersandmaths.js
@@ -0,0 +1,11 @@
+const a=5000;
+
+// console.log(a.toFixed(4));//->point k baaad 4 value tk show krega
+
+// +++++++++++++++++++++MATHS+++
+
+console.log(Math)
+console.log(Math.abs(-4))//convert - to + and + remain +
+console.log(Math.min(4,5,7,1))//to find minimum value
+console.log(Math.max(9,4,17,3))//to find  maximum value
+console.log(Math.random())//provide random value bw 0 and 1
