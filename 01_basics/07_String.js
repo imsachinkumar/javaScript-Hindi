9ab26c9d71ec1d2e2049620706e1dc108f024b3a
imsachinkumar <122716893+imsachinkumar@users.noreply.github.com>
2024-06-24 03:48:42 +0000

    Strings

---
+++ 01_basics/07_String.js
@@ -0,0 +1,16 @@
+const a=new String('Sachin')
+console.log(a[3])//->wiil give letter at third index
+
+console.log(a.length)//will provide length of String
+
+const b=a.substring(0,2)
+console.log(b)
+
+const c="   SCahin   "
+console.log(c.trim()) //remove spaces in a string
+
+
+const d="sachin kumar thakurela is a student"
+console.log(d.replace('is','good'))//to replace
+
+console.log(d.includes('sachin'))//check wheather this word exist in string or not
