b42875f40dd763fb5d3f87a79a383f84499843f6
imsachinkumar <122716893+imsachinkumar@users.noreply.github.com>
2024-06-25 16:22:53 +0000

    date time

---
+++ 01_basics/09_datesandtime.js
@@ -0,0 +1,13 @@
+//DATES
+
+let d=new Date()
+// console.log(d)
+
+// console.log(d.toString())
+// console.log(d.toDateString())
+
+let a=new Date(2023,12,24)
+console.log(a.toDateString())
+
+let b=Date.now()
+console.log(b)//provide time in milisec
