d31f357a4e753cfe5e294f439dcaa2aaa758cc8b
imsachinkumar <122716893+imsachinkumar@users.noreply.github.com>
2024-06-29 03:39:34 +0000

    obj

---
+++ 01_basics/12_objects_2.js
@@ -0,0 +1,33 @@
+// const a={}
+// a.name="Saurya"
+// a.class=6
+// a.id="234dfg"
+// console.log(a)
+
+// const b={
+//     fullname:{
+//         name:"Sachin",
+//         year:2023
+//     }
+// }
+// console.log(b.fullname.name);
+
+//+++++++++++++++++++++++
+
+// const obj1={1 :"a",2:"b"}
+// const obj2={3:"c",4:"d"}
+
+//console.log(obj1,obj2)
+
+// const obj3=Object.assign({},obj1,obj2)
+// console.log(obj3)
+
+//+++++++++++++++++++++
+
+const course={
+    name:"Scahin",
+    pass:999
+}
+const{pass}=course
+// const{pass: p}=course ->it means ab hum pass ko p bhi likh skte h
+console.log(p)
